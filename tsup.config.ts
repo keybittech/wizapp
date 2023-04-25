3c3294be492b0fea661d4b273cf0bad26f9f4237
Joe McCormick <joe.c.mccormick@gmail.com>
2023-04-24 20:38:26 -0700

    official releaseable

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import type { Options } from 'tsup';
+
+const config: Options = {
+  splitting: true,
+  entry: {
+    'lib/index': 'src/lib/index.ts',
+    'server/cli': 'src/server/cli.ts',
+    'server/index': 'src/server/index.ts'
+  },
+  
+  format: ['cjs', 'esm'],
+  outDir: 'dist',
+}
+
+export default config;
